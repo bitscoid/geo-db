9d2ee119bc18bc6448982cd453f8c1961370b93e
BITS <146477031+bitscoid@users.noreply.github.com>
2023-12-17 16:43:04 +0700

    Update release-rule-set-geoip.sh

--- .github/release-rule-set-geoip.sh
+++ .github/release-rule-set-geoip.sh
@@ -6,7 +6,7 @@ cd sing-geoip/rule-set-geoip
 git init
 git config --local user.email "github-action@users.noreply.github.com"
 git config --local user.name "GitHub Action"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/malikshi/sing-box-geo.git
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/bitscoid/geo-db.git
 git branch -M rule-set-geoip
 git add .
 git commit -m "Update rule-set-geoip"
