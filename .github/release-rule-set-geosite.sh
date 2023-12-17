388cdb6be37d2befc1d1a71713e30eeec40791dd
BITS <146477031+bitscoid@users.noreply.github.com>
2023-12-17 16:43:33 +0700

    Update release-rule-set-geosite.sh

--- .github/release-rule-set-geosite.sh
+++ .github/release-rule-set-geosite.sh
@@ -6,7 +6,7 @@ cd sing-geosite/rule-set-geosite
 git init
 git config --local user.email "github-action@users.noreply.github.com"
 git config --local user.name "GitHub Action"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/malikshi/sing-box-geo.git
+git remote add origin https://github-action:$GITHUB_TOKEN@github.com/bitscoid/geo-db.git
 git branch -M rule-set-geosite
 git add .
 git commit -m "Update rule-set-geosite"
